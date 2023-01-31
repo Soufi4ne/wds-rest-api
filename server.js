48456afe91e271cd13af875fea49aca8dfd5955f
Soufi4ne <soufiane.le.breton@efrei.net>
2023-02-01 00:16:17 +0100

    changing port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 //create a express server
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const mongoose = require('mongoose');
 
 //set the "strictQuery" option to "false" and prevent the warning from being displayed.
